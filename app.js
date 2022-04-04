06ece594f50f4884c812414f74b44d07aea207da
deniz karakas <denizkarakas@yaani.com>
2022-04-05 02:15:29 +0300

    partials yapıldı kod temizlendi

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const res = require("express/lib/response");
+const ejs = require("ejs");
 const path = require("path");
 
 const app = express();
+
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
 app.get(`/`, (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+  res.render("index");
+});
+
+app.get(`/about`, (req, res) => {
+  res.render("about");
+});
+
+app.get(`/add`, (req, res) => {
+  res.render("add");
+});
+
+app.get(`/video-page`, (req, res) => {
+  res.render("video-page");
 });
 
 const port = 3000;
